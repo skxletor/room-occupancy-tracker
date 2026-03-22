6d7df29335e6b816602ab10a7a793ea7be424b35
Justin R <jr01@outlook.com>
2026-03-22 15:18:11 -0400

    initial setup for wifi and sensor setup finalized, going to start working on the loop part

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,77 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <VL53L1X.h>
+#include <WiFi.h>
+
+const char* ssid     = "PAWS-Secure";
+const char* identity = "jpr99061";        // just your MyID, e.g. "jr01234"
+const char* username = "jpr99061";        // same as identity
+const char* password = "UniGAJR123$$";
+
+
+const char* serverURL = "url for the esp occupancy";
 
 // put function declarations here:
-int myFunction(int, int);
+VL53L1X sensor1;
+VL53L1X sensor2;
+
+const int SDA_PIN= 21;
+const int SCL_PIN= 22;
+const int XSHUT1_PIN= 23;
+const int XSHUT2_PIN= 32;
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  //wifi section
+  Serial.begin(115200);
+  Serial.println("Connecting to PAWS-Secure...");
+  WiFi.disconnect(true);
+  WiFi.begin(ssid, WPA2_AUTH_PEAP, identity, username, password);
+
+  int counter = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    counter++;
+    if (counter >= 60) {
+      Serial.println("\nTimed out. Restarting...");
+      ESP.restart();
+    }
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+//pin setup, wire setup, etc.
+
+  pinMode(XSHUT1_PIN, OUTPUT);
+  pinMode(XSHUT2_PIN, OUTPUT);
+  digitalWrite(XSHUT1_PIN, LOW);
+  digitalWrite(XSHUT2_PIN, LOW);
+  Wire.begin(SDA_PIN, SCL_PIN);
+  digitalWrite(XSHUT1_PIN, HIGH);
+  delay(10);
+  sensor1.setAddress(0x31);
+  sensor1.init();
+  sensor1.startContinuous(50);
+  digitalWrite(XSHUT2_PIN, HIGH);
+  delay(10);
+  sensor2.setAddress(0x32);
+  sensor2.init();
+  sensor2.startContinuous(50);
+  
+  sensor1.setDistanceMode(VL53L1X::Medium);
+  sensor2.setDistanceMode(VL53L1X::Medium);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  double dist1 = sensor1.ranging_data.range_mm;
+  double dist2 = sensor2.ranging_data.range_mm;
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
