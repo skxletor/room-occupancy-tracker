5dc1c141be33a51e605e42b916a55205a99b388a
Justin R <jr01@outlook.com>
2026-03-24 18:39:20 -0400

    working sensors, sensor 2 is 30mm greater than sensor 1

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ const char* password = "UniGAJR123$$";
 const char* serverURL = "url for the esp occupancy";
 
 // put function declarations here:
+void scanI2C();
+
 VL53L1X sensor1;
 VL53L1X sensor2;
 
@@ -34,26 +36,27 @@ State prevState = IDLE_SHUT;
 void setup() {
   // put your setup code here, to run once:
   
-  //wifi section
   Serial.begin(115200);
-  Serial.println("Connecting to PAWS-Secure...");
-  WiFi.disconnect(true);
-  WiFi.begin(ssid, WPA2_AUTH_PEAP, identity, username, password);
 
-  int counter = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    counter++;
-    if (counter >= 60) {
-      Serial.println("\nTimed out. Restarting...");
-      ESP.restart();
-    }
-  }
+  // //wifi section
+  // Serial.println("Connecting to PAWS-Secure...");
+  // WiFi.disconnect(true);
+  // WiFi.begin(ssid, WPA2_AUTH_PEAP, identity, username, password);
 
-  Serial.println("\nConnected!");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  // int counter = 0;
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  //   counter++;
+  //   if (counter >= 60) {
+  //     Serial.println("\nTimed out. Restarting...");
+  //     ESP.restart();
+  //   }
+  // }
+
+  // Serial.println("\nConnected!");
+  // Serial.print("IP address: ");
+  // Serial.println(WiFi.localIP());
 
 //pin setup, wire setup, etc.
 
@@ -61,27 +64,82 @@ void setup() {
   pinMode(XSHUT2_PIN, OUTPUT);
   digitalWrite(XSHUT1_PIN, LOW);
   digitalWrite(XSHUT2_PIN, LOW);
+
   Wire.begin(SDA_PIN, SCL_PIN);
+  Wire.setClock(100000);
+  delay(50);
+
+  // Bring up sensor 1 and assign address
   digitalWrite(XSHUT1_PIN, HIGH);
   delay(10);
-  sensor1.setAddress(0x31);
-  sensor1.init();
-  sensor1.startContinuous(50);
+  sensor1.setTimeout(500);
+  if (sensor1.init()) {
+    sensor1.setAddress(0x31);
+    sensor1.setDistanceMode(VL53L1X::Medium);
+    sensor1.setMeasurementTimingBudget(50000);
+    sensor1.startContinuous(50);
+    Serial.print("Sensor 1 connected at address 0x");
+    Serial.println(sensor1.getAddress(), HEX);
+  } else {
+    Serial.println("ERROR: Sensor 1 failed to initialize!");
+    Serial.println("Check wiring: SDA=21, SCL=22, XSHUT1=23");
+  }
+
+  // Bring up sensor 2 and assign address
   digitalWrite(XSHUT2_PIN, HIGH);
   delay(10);
-  sensor2.setAddress(0x32);
-  sensor2.init();
-  sensor2.startContinuous(50);
-  
-  sensor1.setDistanceMode(VL53L1X::Medium);
-  sensor2.setDistanceMode(VL53L1X::Medium);
+  sensor2.setTimeout(500);
+  if (sensor2.init()) {
+    sensor2.setAddress(0x32);
+    sensor2.setDistanceMode(VL53L1X::Medium);
+    sensor2.setMeasurementTimingBudget(50000);
+    sensor2.startContinuous(50);
+    Serial.print("Sensor 2 connected at address 0x");
+    Serial.println(sensor2.getAddress(), HEX);
+  } else {
+    Serial.println("ERROR: Sensor 2 failed to initialize!");
+    Serial.println("Check wiring: SDA=21, SCL=22, XSHUT2=32");
+  }
+
+  // Scan I2C bus to verify sensors are actually present
+  Serial.println("\n--- I2C Bus Scan ---");
+  scanI2C();
+  Serial.println("--- End Scan ---\n");
+
   initDist = sensor1.read();
+  Serial.print("Initial distance: ");
+  Serial.println(initDist);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   uint16_t dist1 = sensor1.read();
+  if (sensor1.timeoutOccurred()) {
+    Serial.println("Sensor 1 TIMEOUT");
+  }
+
   uint16_t dist2 = sensor2.read();
+  if (sensor2.timeoutOccurred()) {
+    Serial.println("Sensor 2 TIMEOUT");
+  }
+
+  Serial.print("Sensor 1: ");
+  Serial.print(dist1);
+  Serial.print(" mm  |  Sensor 2: ");
+  Serial.print(dist2);
+  Serial.println(" mm");
+
+  delay(100);
 }
 
 // put function definitions here:
+void scanI2C() {
+  for (uint8_t addr = 1; addr < 127; addr++) {
+    Wire.beginTransmission(addr);
+    uint8_t error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("  Device found at 0x");
+      if (addr < 16) Serial.print("0");
+      Serial.println(addr, HEX);
+    }
+  }
+}
